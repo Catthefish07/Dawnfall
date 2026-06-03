f937d8ab7370e00d27cef16159e419193802dfe8
stardust3785-blip <stardust3785@gmail.com>
2026-06-03 10:47:23 +0800

    Add files via upload

---
+++ inventory.h
@@ -0,0 +1,27 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#include "item.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+static const int maxQuantity = 100; // sets max total of items at 100
+
+class Inventory
+{
+public:
+    Inventory();
+    void addItems(Item);
+    void removeItems(string, int);
+    void useItem(string, class Character*);
+    void setQuantity(string itemName, int qty);
+    int getQuantity(string); // tracks quantity of items in shop
+    void displayInventory();
+    vector<Item> getItems();
+
+private:
+    vector<Item> items;
+};
+
+#endif // INVENTORY_H
