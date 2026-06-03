f60b4016dddd2671faa54d1e4f1644bfb56b5cba
Catthefish07 <catherine.tantono123@gmail.com>
2026-06-03 22:55:05 +0800

    Add getter methods for Skill class attributes

--- skill.cpp
+++ skill.cpp
@@ -2,6 +2,30 @@
 
 Skill::Skill(std::string name, SkillType type, int power, int cooldown) : name(name), type(type), power(power), maxCooldown(cooldown), currentCooldown(0) {}
 
+std::string Skill::getName() const{
+    return name;
+}
+
+SkillType Skill::getType() const
+{
+    return type;
+}
+
+int Skill::getPower() const
+{
+    return power;
+}
+
+int Skill::getMaxCooldown() const
+{
+    return maxCooldown;
+}
+
+int Skill::getCurrentCooldown() const
+{
+    return currentCooldown;
+}
+
 bool Skill::isReady() const {
     return currentCooldown == 0;
 }
