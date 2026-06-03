da7c917debb313699782688e3db7cd07f9a0585e
Catthefish07 <catherine.tantono123@gmail.com>
2026-06-03 22:51:25 +0800

    Refactor Boss class methods and cooldown logic

--- boss.cpp
+++ boss.cpp
@@ -1,14 +1,16 @@
 #include "boss.h"
 #include "character.h"
 
-Boss::Boss(string enemyName, int enemyHP, int enemyAttack, int enemyDefense, int enemySpeed, int enemyCoinDrop) : Enemy(enemyName, enemyHP, enemyAttack, enemyDefense, enemySpeed, enemyCoinDrop), currentAoeCooldown(0), currentHealCooldown(0) {}
+Boss::Boss(std::string enemyName, int enemyHP, int enemyAttack, int enemyDefense, int enemySpeed, int enemyCoinDrop) : Enemy(enemyName, enemyHP, enemyAttack, enemyDefense, enemySpeed, enemyCoinDrop), currentAoeCooldown(0), currentHealCooldown(0) {}
 
 EnemyAction Boss::chooseAction(const vector<Character*>&) {
+    // if the boss' HP is below 30% it will self heal
     if (HP <= maxHP * 0.30 && currentHealCooldown == 0) {
         currentHealCooldown = HEAL_COOLDOWN;
         return ENEMY_HEAL;
     }
 
+    // if the cooldown is off then use this smash AOE
     if (currentAoeCooldown == 0) {
         currentAoeCooldown = AOE_COOLDOWN;
         return AOE_ATTACK;
@@ -17,20 +19,32 @@ EnemyAction Boss::chooseAction(const vector<Character*>&) {
     return BASIC_ATTACK;
 }
 
+// How the boss works here is that it would go for the highest ATK hero
 Character* Boss::chooseTarget(const vector<Character*>& party) {
-    Character* target = nullptr;
-
-    for (Character* member : party) {
-        if (!member->isAlive()) continue;
-
-        if (!target || member->getAttack() > target->getAttack()) {
-            target = member;
+    std::vector<Character*> alive;
+    for (Character* c : party) {
+        if (c->isAlive()){
+            alive.push_back(c);
         }
     }
+    if (alive.empty())
+        return nullptr;
 
-    return target;
+    if (rand() % 100 < 70) {
+        Character* target = alive[0];
+        for (Character* c : alive) {
+            if (c->getAttack() > target->getAttack()){
+                target = c;
+            }
+        }
+        return target;
+    }
+    else {
+        return alive[rand() % alive.size()];
+    }
 }
 
+// Thus function will be called at the start of the boss' turn to count its cooldowns down
 void Boss::reduceCooldowns() {
     if (currentAoeCooldown > 0){
         currentAoeCooldown--;
