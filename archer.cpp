2fb319ec1b72545025f848326a8c12b2763f6e0b
Catthefish07 <catherine.tantono123@gmail.com>
2026-06-03 22:49:18 +0800

    Refactor Archer class to inherit from RangeAttack
    also changed the dealdamage to have a multiplier

--- archer.cpp
+++ archer.cpp
@@ -1,9 +1,9 @@
 #include "archer.h"
 
-Archer::Archer(std::string name, int maxHP, int attack, int defense, int speed) : Character(name, maxHP, attack, defense, speed) {
-    skills.push_back(Skill("Precise Shot", DAMAGE,  35, 2));
+Archer::Archer(std::string name, int maxHP, int attack, int defense, int speed) : RangeAttack(name, maxHP, attack, defense, speed) {
+    skills.push_back(Skill("Precise Shot", DAMAGE,  20, 2));
 }
 
 int Archer::dealDamage() const {
-    return attack;
+    return (int)(attack * 1.3);
 }
