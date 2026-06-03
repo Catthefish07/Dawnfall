a0133ea2d5f17ba29507e5e41e27d80f799ce694
Catthefish07 <catherine.tantono123@gmail.com>
2026-06-03 22:55:54 +0800

    Refactor Tank class to inherit from MeleeAttack

--- tank.cpp
+++ tank.cpp
@@ -1,9 +1,9 @@
 #include "tank.h"
 
-Tank::Tank(std::string name, int maxHP, int attack, int defense, int speed) : Character(name, maxHP, attack, defense, speed) {
-    skills.push_back(Skill("Sun Guard", DEFEND, 20, 4));
+Tank::Tank(std::string name, int maxHP, int attack, int defense, int speed) : MeleeAttack(name, maxHP, attack, defense, speed) {
+    skills.push_back(Skill("Sun Guard", DEFEND, 12, 4));
 }
 
 int Tank::dealDamage() const {
-    return attack;
+    return (int)(attack * 0.7);
 }
