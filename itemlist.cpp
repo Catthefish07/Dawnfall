68202441da193fd670bfb3740899bb58df66414e
stardust3785-blip <stardust3785@gmail.com>
2026-06-03 15:20:29 +0800

    Add files via upload

---
+++ itemlist.cpp
@@ -0,0 +1,12 @@
+#include "itemlist.h"
+#include <vector>
+using namespace std;
+
+vector<Item> listOfItems() {
+    return {
+        Item("Health Potion", 0, 0, 35, 1500, false),
+        Item("Mega Potion", 0, 0, 80, 3000, false),
+        Item("Revive Stone", 0, 0, 0, 5000, false)
+    };
+}
+// default, start from 0, not owned, fixed items
