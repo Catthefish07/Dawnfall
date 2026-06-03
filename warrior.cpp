56eb3db642bd852fc7165030b0ca5298674ac02f
Catthefish07 <catherine.tantono123@gmail.com>
2026-06-03 22:56:35 +0800

    Refactor Warrior class for improved damage calculation

--- warrior.cpp
+++ warrior.cpp
@@ -3,10 +3,10 @@
 #include <string>
 #include <cstdlib>
 
-Warrior::Warrior(std::string name, int maxHP, int attack, int defense, int speed) : Character(name, maxHP, attack, defense, speed) {
-    skills.push_back(Skill("Power Strike", DAMAGE, 15, 3));
+Warrior::Warrior(std::string name, int maxHP, int attack, int defense, int speed) : MeleeAttack(name, maxHP, attack, defense, speed) {
+    skills.push_back(Skill("Power Strike", DAMAGE, 25, 3));
 }
 
 int Warrior::dealDamage() const {
-    return attack;
+    return (int)(attack * 1.2);
 }
