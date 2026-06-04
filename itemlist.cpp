c700023742e5f5b13a5d989d3d116be688495705
Catthefish07 <catherine.tantono123@gmail.com>
2026-06-04 08:59:04 +0800

    Modify item properties in listOfItems function
    Updated item attributes for Health Potion, Mega Potion, and Revive Stone.

--- itemlist.cpp
+++ itemlist.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 vector<Item> listOfItems() {
     return {
-        Item("Health Potion", 0, 0, 50, 80, false),
-        Item("Mega Potion", 0, 0, 150, 200, false),
-        Item("Revive Stone", 0, 0, 0, 400, false)
+        Item("Health Potion", 0, 0, 35, 1500, false),
+        Item("Mega Potion", 0, 0, 80, 3000, false),
+        Item("Revive Stone", 0, 0, 0, 5000, false)
     };
 }
 // default, start from 0, not owned, fixed items
