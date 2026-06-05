946f07d51db6385fbadba82974c61caba8aedc37
stardust3785-blip <stardust3785@gmail.com>
2026-06-05 20:07:11 +0800

    Add files via upload

---
+++ character.h
@@ -0,0 +1,51 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <string>
+#include <vector>
+#include "skill.h"
+
+enum AttackType { MELEE, RANGE };
+
+class Character {
+protected:
+    std::string name;
+    std::vector<Skill> skills;
+    int level;
+    int HP;
+    int maxHP;
+    int attack;
+    int defense;
+    int speed;
+    int defenseBuff = 0;
+    int defenseBuffDuration = 0;
+
+public:
+    Character(std::string name, int maxHP, int attack, int defense, int speed);
+    virtual ~Character() = default;
+
+    void takeDamage(int amount);
+    virtual int dealDamage() const = 0;
+    virtual AttackType getAttackType() const = 0;
+    void levelUp();
+
+    void setLevel(int lvl);
+
+    void heal(int amount);
+    bool isAlive() const;
+
+    std::string getName() const;
+    int getHP() const;
+    int getMaxHP() const;
+    int getAttack() const;
+    int getDefense() const;
+    int getSpeed() const;
+    int getLevel() const;
+
+    std::vector<Skill>& getSkills();
+    virtual void tickSkills();
+    void addTempDefense(int amount, int turns);
+
+};
+
+#endif
---
+++ savemanager.h
@@ -0,0 +1,54 @@
+#ifndef SAVEMANAGER_H
+#define SAVEMANAGER_H
+
+#include "inventory.h"
+#include "shop.h"
+#include "partymanager.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct PlayerRecord
+{
+    char UID[8];
+    char playerUsername[25];
+    int avatarIndex = 0;
+    int coins;
+    int currentChapter; // last saved chapter
+    int characterLevels[9]; // each character levels
+    bool characterUnlocked[9]; //  true if unlocked, false if locked
+    bool occupied; // slot has save or not
+    int partyIndexes[4]; // character currently in party
+};
+
+class SaveManager
+{
+public:
+    int generateUID();
+
+    void saveGame(int slot, PlayerRecord player, Inventory& iv, Shop& shop, PartyManager& pm); // saves data of the whole game
+    PlayerRecord loadGame(int slot, Inventory& iv, Shop& shop, PartyManager& pm); // restores back data when player loads game
+
+    bool isSlotEmpty(int slot);
+    void deleteSlot(int slot); // erase slot
+
+    void saveInventory(Inventory& iv); // save inventory quantities to items.dat
+    void loadInventory(Inventory& iv); // restores
+
+    void addBattleLog(string text); // adds a line like "Snake defeated. Coins +..."
+    vector<string> readBattleLog();
+
+    void addStoryLog(string text); // adds a line of text, when character joined the party, chapter 1 accomplished.."
+    vector<string> readStoryLog();
+
+private:
+    string saveFile = "players.dat"; // saves player's account data/progress data
+    string itemFile = "items.dat"; // for inventory quantities
+    string battleFile = "battlelog.txt"; // text file for battle history
+    string storyFile = "storylog.txt"; // text file for story progress history
+
+    int totalSlots = 3; // totalslots to save progress on story mode
+};
+
+#endif // SAVEMANAGER_H
